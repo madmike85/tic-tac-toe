93374c71e69dee124a5f7dc448f997690086507f
Mikhail Shcheglakov <m.shcheglakov@gmail.com>
2019-09-25 22:06:52 +0300

    feat: add task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,60 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.game = [[null, null, null], [null, null, null], [null, null, null]];
+    this.player = 'x';
+    this.turns = 9;
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.player;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (!this.game[rowIndex][columnIndex]) {
+      this.game[rowIndex][columnIndex] = this.player;
+      this.player = this.player === 'x' ? 'o' : 'x';
+      this.turns--;
+      if (this.turns < 0) this.turns = 0;
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    return Boolean(this.getWinner()) || this.noMoreTurns();
+  }
 
+  findWinner(player) {
+    if (
+      (this.game[0][0] === player && this.game[0][1] === player && this.game[0][2] === player) ||
+      (this.game[1][0] === player && this.game[1][1] === player && this.game[1][2] === player) ||
+      (this.game[2][0] === player && this.game[2][1] === player && this.game[2][2] === player) ||
+      (this.game[0][0] === player && this.game[1][0] === player && this.game[2][0] === player) ||
+      (this.game[0][1] === player && this.game[1][1] === player && this.game[2][1] === player) ||
+      (this.game[0][2] === player && this.game[1][2] === player && this.game[2][2] === player) ||
+      (this.game[0][0] === player && this.game[1][1] === player && this.game[2][2] === player) ||
+      (this.game[0][2] === player && this.game[1][1] === player && this.game[2][0] === player)
+    ) {
+      return player;
     }
+    return false;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  getWinner() {
+    if (this.findWinner('x')) return 'x';
+    else if (this.findWinner('o')) return 'o';
+    else return null;
+  }
 
-    }
+  noMoreTurns() {
+    return this.turns === 0;
+  }
 
-    isFinished() {
+  isDraw() {
+    return this.isFinished() && !this.getWinner();
+  }
 
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.game[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
